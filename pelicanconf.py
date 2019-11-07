b621d018c9eed0c0ab3a524b31a17f2308ce0d13
s40723118 <s40723118@gm.nfu.edu.tw>
2019-11-07 23:26:18 +0800

    11.7-test

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = '第二組'
-SITENAME = '電腦輔助設計實習課程網頁'
+AUTHOR = 'group2'
+SITENAME = 'cad2019'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
